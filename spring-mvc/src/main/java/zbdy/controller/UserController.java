969724bf4c396965315bbeea06fa9e6ce0067444
Ben Zhang <zbdy20061001@163.com>
2017-08-11 08:35:53 +0800

    Add spring-mvc

---
+++ spring-mvc/src/main/java/zbdy/controller/UserController.java
@@ -0,0 +1,17 @@
+package zbdy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	   @RequestMapping(method = RequestMethod.GET)
+	   public String printHello(ModelMap model) {
+	      model.addAttribute("message", "Hello Spring MVC Framework!");
+
+	      return "user";
+	   }
+}
