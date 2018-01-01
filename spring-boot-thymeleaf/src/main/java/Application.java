af8620e3e0669f293a6813b6795981e00299e414
Ben Zhang <zbdy20061001@163.com>
2018-01-01 18:19:49 +0800

    Add spring boot

---
+++ spring-boot-thymeleaf/src/main/java/Application.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages="zbdy")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ spring-restful/readme.txt
@@ -0,0 +1,1 @@
+This is a pure non xml restful project
