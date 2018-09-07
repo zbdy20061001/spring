e987ace39e3ece425695cb914fac2fbf468e6911
Ben Zhang <zbdy20061001@163.com>
2018-09-08 01:50:22 +0800

    some update

--- spring-boot-data/src/main/java/zbdy/dao/UserRepository.java
+++ spring-boot-data/src/main/java/zbdy/dao/UserRepository.java
@@ -8,9 +8,13 @@ import zbdy.model.User;
 
 //public interface UserRepository extends CrudRepository<User, Integer>{
 //PagingAndSortingRepository extends CrudRepository
-public interface UserRepository extends PagingAndSortingRepository<User, Integer>{
+public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
 
 	List<User> findByUsername(String username);
-	
+
+	// derivation method, such as countByXXX, deleteByXXX, removeByXXX
+	// long deleteByLastname(String lastname);
+	// List<User> removeByLastname(String lastname);
+	//Refer to Spring JPA Doc 2.4.2. Query Creation
 	long countByUsername(String username);
 }
